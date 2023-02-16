39ead45b142478f037583c64105086feca93e992
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2023-02-16 21:26:48 +0100

    Fully working Game implementation

--- src/jsMain/kotlin/Game.kt
+++ src/jsMain/kotlin/Game.kt
@@ -1,20 +1,69 @@
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import components.Circle
+import components.Cross
 import components.Footer
 import components.Header
-import org.jetbrains.compose.web.css.gridTemplateColumns
-import org.jetbrains.compose.web.css.gridTemplateRows
+import kotlinx.browser.document
+import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 
-const val fieldSize = 3
+const val fieldSize: Int = 3
+var isPlayable: Boolean = true;
 
-val field: List<String> = listOf(
-    "", "", "",
-    "", "", "",
-    "", "", "",
-)
+fun checkForWinner(field: SnapshotStateList<String>): String? {
+    val winningPaths: List<List<Int>> = listOf(
+        listOf(0, 1, 2),
+        listOf(3, 4, 5),
+        listOf(6, 7, 8),
+        listOf(0, 3, 6),
+        listOf(1, 4, 7),
+        listOf(2, 5, 8),
+        listOf(0, 4, 8),
+        listOf(2, 4, 6),
+    )
+
+    for (path in winningPaths) {
+        val checkPlayer: String? = if(field[path[0]] != "") field[path[0]] else null
+
+        var playerHasRow = true;
+
+        for(block in path) {
+            if(field[block] != checkPlayer) {
+                playerHasRow = false;
+            }
+        }
+
+        if(playerHasRow) {
+            for(i in 0 until 9) {
+                val playerField = document.getElementById("player-button-$i")
+
+                playerField?.classList?.add("unselectable")
+
+                if(i in path) {
+                    playerField?.classList?.add("winning-field")
+                }
+            }
+
+            return checkPlayer
+        }
+    }
+
+    return null
+}
 
 @Composable
 fun Game() {
+    val field = remember {
+        mutableStateListOf(
+            "", "", "",
+            "", "", "",
+            "", "", ""
+        )
+    }
+
+    val playerTurn = remember { mutableStateOf("X") }
+
     Div({
         classes("game-page")
     }) {
@@ -23,6 +72,10 @@ fun Game() {
         Div({
             classes("game-container")
         }) {
+            Span({ classes("game-title") }) {
+                Text("Tic Tac Toe 2-Player")
+            }
+
             Div({
                 classes("player-field")
 
@@ -31,21 +84,34 @@ fun Game() {
                     gridTemplateRows("repeat(${fieldSize}, 80px)")
                 }
             }) {
-                for (i in 1..fieldSize * fieldSize) {
+                for (i in 0 until 9) {
                     Div({
+                        id("player-button-$i")
                         classes("player-button")
 
-                        if(field[i] != "") {
-                            classes("unselectable")
-                        }
-
                         onClick {
-                            // TODO: Detect Player Move
+                            if (field[i] == "" && isPlayable) {
+                                field[i] = playerTurn.value
+
+                                playerTurn.value = if (playerTurn.value == "X") "O" else "X"
+
+                                val winner = checkForWinner(field)
+
+                                if (winner != null) {
+                                    isPlayable = false
+                                }
+                            }
                         }
                     }) {
-
+                        if (field[i] == "X") {
+                            Cross()
+                        }
+                        else if (field[i] == "O") {
+                            Circle()
+                        }
                     }
                 }
+
             }
         }
 
---
+++ src/jsMain/kotlin/components/Circle.kt
@@ -0,0 +1,9 @@
+package components
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.dom.Img
+
+@Composable
+fun Circle() {
+    Img("static/circle.svg")
+}
